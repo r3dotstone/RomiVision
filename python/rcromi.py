e59f1515533584609d483698abb500feaf08d306
fabensp <fabensp@lafayette.edu>
2023-11-28 21:02:53 -0500

    debug

--- python/rcromi.py
+++ python/rcromi.py
@@ -24,7 +24,7 @@ def status():
     odo.updateOdometry(encoders[0], encoders[1])
     data = {
         "encoders": encoders,
-        "odometry": [odo.dt, odo.u]
+        "odometry": [odo.prev_t, odo.u]
     }
     return json.dumps(data)
 
