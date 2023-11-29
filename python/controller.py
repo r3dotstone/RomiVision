5c289f1564e5ed5d0ecaf6d93613f644cf36d8ce
fabensp <fabensp@lafayette.edu>
2023-11-28 20:22:00 -0500

    adapted odometry to be a class

--- python/controller.py
+++ python/controller.py
@@ -1,41 +1,42 @@
+import time as time
+import numpy as np
 
-
-#values for getting joint velocities
-oldLeftEncoder = 0#value for old left encoder (for velocity computation)
-oldRightEncoder = 0 #old right encoder
-
-#values to hold velocities
-omega_left = 0
-omega_right = 0
-
-#variables to hold allocentric romi estimates
-Xromi = 0
-Yromi = 0
-#variables to hold egocentric romi estimates
-U = 0
-psidot = 0
-psi = 0
-
-# romi parameters
 r_romi = .07438
 r_wheel = .035
 
-def updateQdots():
-    global oldRightEncoder,oldLeftEncoder,omega_right,omega_left
-    omega_right = (romi.encRight-oldRightEncoder)/(timestep/1000.0) / (120 * 12) * (2 * 3.1415)
-    omega_left = (romi.encRight - oldLeftEncoder)/(timestep/1000.0) / (120 * 12) * (2 * 3.1415)
+class Odometer:
+    def __init__(self):
+        self.enc_l_old = 0
+        self.enc_r_old = 0
 
-def updateVelocities():
-    global omega_left,omega_right,r_romi,psidot,U
-    vLeft = omega_right*r_romi
-    vRight = omega_right*r_romi
-    U = (vLeft + vRight) / 2
-    psidot = (vRight - vLeft) / (2 * r_romi)
+        self.wl = 0
+        self.wr = 0
 
-def updateOdometry():
-    global Xromi,Yromi,psi,psidot,timestep,U
-    #use to get estimates of current romi X, and Y in allocentric frame
-    #using odometry:
-    Xromi = Xromi + U * np.cos(psi) * timestep/1000.0 #TODO implement this!
-    Yromi = Yromi + U * np.sin(psi) * timestep/1000.0 #TODO implement this!
-    psi = psi + timestep/1000.0*psidot
+        self.x = 0
+        self.y = 0
+
+        self.u = 0
+        self.psidot = 0
+        self.psi = 0
+
+        self.prev_t = 0
+
+    def _updateQdots(self, enc_r, enc_l, dt):
+        self.wr = (enc_r - self.enc_r_old)/ dt / (120 * 12) * (2 * 3.1415)
+        self.wl = (enc_l - self.enc_r_old)/ dt / (120 * 12) * (2 * 3.1415)
+
+    def _updateVelocities(self):
+        vl = self.wr*r_romi
+        vr = self.wl*r_romi
+        U = (vl + vr) / 2
+        psidot = (vr - vl) / (2 * r_romi)
+
+    def updateOdometry(self, enc_r, enc_l):
+        t = time.process_time()
+        dt = t - self.prev_t
+        self._updateQdots(enc_r, enc_l, dt)
+        self._updateVelocities()
+        self.x = self.x + self.u * np.cos(psi) * dt
+        self.y  = self.y + self.u * np.sin(psi) * dt
+        psi = psi + dt*self.psidot
+        self.prev_t = t
