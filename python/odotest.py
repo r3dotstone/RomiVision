dce05c7219f6fe09169903cfb6562fe194dcfae9
fabensp <fabensp@lafayette.edu>
2023-11-30 19:34:20 -0500

    debug

--- python/odotest.py
+++ python/odotest.py
@@ -9,8 +9,8 @@ odo = Odometer(romi.read_encoders())
 start = time.time()
 
 while (time.time() - start < 3):
-    romi.motors(255, 230)
+    romi.motors(255, 255)
     encoders = romi.read_encoders()
     odo.updateOdometry(encoders[0], encoders[1])
-    print(time.time() - start, odo.x, odo.y)
+    print(time.time() - start, odo.enc_l_old, odo.enc_r_old)
 romi.motors(0, 0)
