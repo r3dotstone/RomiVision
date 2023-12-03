8477b7891920838173b40b490209cc37d753a1d5
r3dotstone <jstone.import@gmail.com>
2023-12-03 16:40:01 -0500

    cleaning

--- cvTest.py
+++ cvTest.py
@@ -1,18 +1,17 @@
-# importing required libraries of opencv 
 import cv2 
-# import sys
-# sys.path.append("/pics")
-
-# importing library for plotting 
+import numpy as np
 from matplotlib import pyplot as plt 
   
 # reads an input image 
-img = cv2.imread("test1.jpg",0) 
+img = cv2.imread("./pics/yellowsquare.jpg",0) 
   
 # find frequency of pixels in range 0-255 
-histr = cv2.calcHist([img],[0],None,[256],[0,256]) 
-thresh = 2e5
+thresh = 250e3
+while True:
+    histr = cv2.calcHist([img],[0],None,[256],[0,256]) 
+    histrGtThresh = histr[histr > thresh]
+    if (histr.size/histrGtThresh.size) > 20:
+        print("goin forward...")
 # show the plotting graph of an image 
 plt.plot(histr)
-print(histr)
 plt.show()
