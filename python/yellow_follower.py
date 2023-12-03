674077b6742ebc3de456e428d3b9944c1dd2bb99
fabensp <fabensp@lafayette.edu>
2023-12-03 17:30:51 -0500

    change err calc

--- python/yellow_follower.py
+++ python/yellow_follower.py
@@ -22,28 +22,20 @@ params.minArea = 10
   
 params.filterByCircularity = True 
 params.minCircularity = 0.6
-#params.maxCircularity = 0.85
+params.maxCircularity = 0.85
 
 detector = cv2.SimpleBlobDetector_create(params)
 
 while (True):
     image = picam2.capture_array("main")
 
-    #image = np.frombuffer(cameraData, np.uint8).reshape((cHeight, cWidth, 4))
-    #convert to HSV
     hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-    #Threshold GREEEN STUFFFFFFF
     lower_bound = np.array([20,0,0])
     upper_bound = np.array([70,255,255])
-    #create a mask of pixels in this range
     mask = cv2.inRange(hsv,lower_bound,upper_bound)
-    #now we use mask to threshold our image!
     thresh = cv2.bitwise_and(image,image,mask=mask)
-    #now convert to grayscale for centroid calc
     grey = cv2.cvtColor(thresh,cv2.COLOR_BGR2GRAY)
-    #now find centroid! Use the moments function
     M = cv2.moments(mask)
-    #to find the centroid...
     if(M["m00"] != 0):
         cX = int(M["m10"]/M["m00"])
         cY = int(M["m01"]/M["m00"])
@@ -63,13 +55,11 @@ while (True):
     print(lCmd, rCmd, e)
     romi.motors(lCmd, rCmd)
 
-    #add a label to original image!!
     cv2.circle(image,(cX,cY),5,(0,0,255),-1)
     cv2.putText(image,"centroid",(cX-25,cY-25),
     cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2)
     blank = np.zeros((1, 1))  
     cv2.drawKeypoints(image, keypoints, blank, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) 
 
-    #display camera Image
     cv2.imshow("camera",image)
     cv2.waitKey(1)
