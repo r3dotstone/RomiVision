b15c4df97944cdf370448bcc09106122a765a386
fabensp <fabensp@lafayette.edu>
2023-12-03 18:06:25 -0500

    add dbg print

--- python/yellow_follower.py
+++ python/yellow_follower.py
@@ -48,11 +48,11 @@ while (True):
     lCmd = 0
     e = 0
     for kp in keypoints:
-        e = int((cX - cWidth/2) * 150 / cWidth*2)
+        e = int((kp.pt[0] - cWidth/2) * 150 / cWidth*2)
         lCmd = np.clip(150 + e, -255, 255)
         rCmd = np.clip(150 - e, -255, 255)
 
-    print(lCmd, rCmd, e)
+    print(lCmd, rCmd, e, keypoints[0].pt[0], keypoints[0].pt[1])
     romi.motors(lCmd, rCmd)
 
     cv2.circle(image,(cX,cY),5,(0,0,255),-1)
